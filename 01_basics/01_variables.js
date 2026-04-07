9b3bc6c53285672e44e1ef6a85b04d899a7082d1
VAIBHAV RAVINDRAGIR BAWA <120723761+vaibhavbawa@users.noreply.github.com>
2026-04-07 10:37:29 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "vaibhav@gmail.com"
+var accountPassword = "123456"
+accountCity = "Nandurbar"
+let accountState;
+
+// accountId = 121212;
+// accountEmail = "pratish@gmail.com"
+// accountPassword = "21212121"
+// accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
