ffa581a726c2173abd0170ab229cf32f8b7e4298
VAIBHAV RAVINDRAGIR BAWA <120723761+vaibhavbawa@users.noreply.github.com>
2026-04-10 11:55:59 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "Vaibhav"
+const repo = 19
+
+// console.log(name + repo + "Value");
+
+// console.log(`Hellow my name is ${name} and my repo count is ${repo}`);
+
+const gameName = new String('Pratish')
+// console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = " Vaibhav "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://vaibhav.com/vaibhav%20bawa";
+// console.log(url.replace(
+
+
+
+
+
+
+
