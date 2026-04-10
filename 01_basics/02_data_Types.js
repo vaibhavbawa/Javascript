6f5439225734a3a6fab33aca13dd1b6b92a563c6
VAIBHAV RAVINDRAGIR BAWA <120723761+vaibhavbawa@users.noreply.github.com>
2026-04-10 05:41:01 +0000

    data types

---
+++ 01_basics/02_data_Types.js
@@ -0,0 +1,27 @@
+"use strict"; // treat Js code as newer version 
+// alert(3+2) // we are using nodejs, not browser
+
+console.log(3
+    +
+3) // Code readability should be high
+
+console.log("Vaibhav");
+
+
+let name = "vaibhav";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigInt 
+// string => "------"
+// boolean => true/false 
+// null => standlone value
+// undefined => 
+// symbol =? unique 
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null ); // object 
+
