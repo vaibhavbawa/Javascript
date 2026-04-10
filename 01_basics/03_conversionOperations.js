b689e792b10ed6c9b12073890e04ab3b41d56ecc
VAIBHAV RAVINDRAGIR BAWA <120723761+vaibhavbawa@users.noreply.github.com>
2026-04-10 06:15:50 +0000

    Conversion Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefinde => NaN
+
+
+let isLoggedIn = "Vaibhav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "Vaibhav" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
