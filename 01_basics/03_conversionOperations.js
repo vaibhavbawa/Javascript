c68b993cfd512489ce39c3ec54d1fe5dd190145f
VAIBHAV RAVINDRAGIR BAWA <120723761+vaibhavbawa@users.noreply.github.com>
2026-04-10 06:47:21 +0000

    Conversion Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -18,7 +18,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Vaibhav"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false 
 // "" => false 
@@ -27,8 +27,58 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// *********************** Operations ***********************
+
+let value = 3 
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(3-3);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Vaibhav"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// console.log((3 + 4 )* 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
 
 
 
